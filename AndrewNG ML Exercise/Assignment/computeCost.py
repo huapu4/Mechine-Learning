496b2a6e77e7ab21b9edf23c329fc6e8953de0e0
huapu4 <35124625+huapu4@users.noreply.github.com>
2019-01-21 01:28:12 +0800

    Delete computeCost.py

--- AndrewNG ML Exercise/Assignment/computeCost.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-
-def compute_cost(X, y, theta):
-    m = y.size #数据的个数
-    cost = 0    
-    cost = np.sum(np.power(((np.dot(X, theta)) - y), 2)) / (2 * m) #代价函数
-    return cost
