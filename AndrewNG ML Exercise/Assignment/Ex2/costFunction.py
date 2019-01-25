12d725691b99f87ba04a3aebfca35482823742a3
huapu4 <35124625+huapu4@users.noreply.github.com>
2019-01-25 22:35:24 +0800

    Update costFunction.py

--- AndrewNG ML Exercise/Assignment/Ex2/costFunction.py
+++ AndrewNG ML Exercise/Assignment/Ex2/costFunction.py
@@ -4,19 +4,12 @@ from sigmoid import *
 
 def cost_function(theta, X, y):
     m = y.size
-
-    # You need to return the following values correctly
     cost = 0
     grad = np.zeros(theta.shape)
     
-
-    # ===================== Your Code Here =====================
-    # Instructions : Compute the cost of a particular choice of theta
-    #                You should set cost and grad correctly.
-    #
-    # ===========================================================
-    h = sigmoid(X.dot(theta))
-    first = -y * np.log(h)
+    h = sigmoid(X.dot(theta))  ##hypothesis
+    ##  the cost function
+    first = -y * np.log(h)     
     second = (1-y) * np.log(1-h)
     cost = np.sum(first - second) / m
 
