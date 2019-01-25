977bc6639af8e3e38904cbed8ebd1b47d17142e4
huapu4 <35124625+huapu4@users.noreply.github.com>
2019-01-25 22:38:38 +0800

    Update predict.py

--- AndrewNG ML Exercise/Assignment/Ex2/predict.py
+++ AndrewNG ML Exercise/Assignment/Ex2/predict.py
@@ -5,15 +5,8 @@ from sigmoid import *
 def predict(theta, X):
     m = X.shape[0]
 
-    # Return the following variable correctly
     p = np.zeros(m)
 
-    # ===================== Your Code Here =====================
-    # Instructions : Complete the following code to make predictions using
-    #                your learned logistic regression parameters.
-    #                You should set p to a 1D-array of 0's and 1's
-    #
-    # ===========================================================
     p = sigmoid(X.dot(theta))
     positive = np.where(p >= 0.5)
     negative = np.where(p < 0.5)
