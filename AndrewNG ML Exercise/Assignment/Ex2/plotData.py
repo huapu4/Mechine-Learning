90672698e431fc4c4fa9e636a7f042a3361835e7
huapu4 <35124625+huapu4@users.noreply.github.com>
2019-01-25 22:38:01 +0800

    Update plotData.py

--- AndrewNG ML Exercise/Assignment/Ex2/plotData.py
+++ AndrewNG ML Exercise/Assignment/Ex2/plotData.py
@@ -8,10 +8,3 @@ def plot_data(X, y):
     
     plt.scatter(X[positive, 0], X[positive, 1], c='b', marker='x')
     plt.scatter(X[negative, 0], X[negative, 1], c='y', marker='o')
-    
-    # ===================== Your Code Here =====================
-    # Instructions : Plot the positive and negative examples on a
-    #                2D plot, using the marker="+" for the positive
-    #                examples and marker="o" for the negative examples
-    #
-
