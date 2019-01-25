abccafd8aef40c15cf78de103b63a450e645314b
huapu4 <35124625+huapu4@users.noreply.github.com>
2019-01-25 22:37:04 +0800

    Update costFunctionReg.py

--- AndrewNG ML Exercise/Assignment/Ex2/costFunctionReg.py
+++ AndrewNG ML Exercise/Assignment/Ex2/costFunctionReg.py
@@ -4,19 +4,12 @@ from sigmoid import *
 
 def cost_function_reg(theta, X, y, lmd):
     m = y.size
-
-    # You need to return the following values correctly
     cost = 0
     grad = np.zeros(theta.shape)
-
-    # ===================== Your Code Here =====================
-    # Instructions : Compute the cost of a particular choice of theta
-    #                You should set cost and grad correctly.
-    #
-    # ===========================================================
     
-    h = sigmoid(X.dot(theta))
+    h = sigmoid(X.dot(theta))    ## hypothesis
     reg_theta = theta[1:] #从1开始才需要加入正则项
+    ## the cost function with regularization
     first = -y * np.log(h)
     second = (1-y) * np.log(1-h)
     reg = lmd * np.sum(reg_theta * reg_theta) / (2 * m)
