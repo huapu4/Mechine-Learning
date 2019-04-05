6375da24298d218f2bee9a7ce3130fbddf670e55
huapu4 <35124625+huapu4@users.noreply.github.com>
2019-04-05 22:29:53 +0800

    Add files via upload

---
+++ AndrewNG ML Exercise/Assignment/Ex7/computeCentroids.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+
+def compute_centroids(X, idx, K):
+    # Useful values
+    (m, n) = X.shape
+
+    # You need to return the following variable correctly.
+    centroids = np.zeros((K, n))
+
+    # ===================== Your Code Here =====================
+    # Instructions: Go over every centroid and compute mean of all points that
+    #               belong to it. Concretely, the row vector centroids[i]
+    #               should contain the mean of the data points assigned to
+    #               centroid i.
+    #
+    # ==========================================================
+    
+    for k in range(K):
+        k_x = X[np.where(idx == k)]
+        centroid = np.sum(k_x, axis=0) / k_x.shape[0]
+        centroids[k] = centroid
+    
+    return centroids
